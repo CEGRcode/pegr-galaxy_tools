d0066ba62c63fcb62285f30ab23e44699f0d9c27
jcha40 <sungsoocha97@gmail.com>
2023-11-22 11:54:12 -0500

    Updated meme output stats to fit new pegr specifications

--- tools/meme_meme_output_stats.py
+++ tools/meme_meme_output_stats.py
@@ -8,10 +8,13 @@ parser.add_argument('--history_id', dest='history_id', help='History id')
 parser.add_argument('--history_name', dest='history_name', help='History name')
 parser.add_argument('--input_html', dest='input_htmls', action='append', nargs=5, help='HTML input datasets and attributes')
 parser.add_argument('--input_txt', dest='input_txts', action='append', nargs=5, help='Text input datasets and attributes')
+parser.add_argument('--input_forward_svg', dest='input_forward_svgs', action='append', nargs=5, help='Forward strand SVG input dataset')
+parser.add_argument('--input_reverse_svg', dest='input_reverse_svgs', action='append', nargs=5, help='Reverse strand SVG input dataset')
 parser.add_argument('--output', dest='output', help='Output dataset')
 parser.add_argument('--stats_tool_id', dest='stats_tool_id', help='The caller of this script')
 parser.add_argument('--stderr', dest='stderr', help='Job stderr')
 parser.add_argument('--tool_id', dest='tool_id', help='Tool that was executed to produce the input dataset')
+parser.add_argument('--tool_category', dest='tool_category', help='Tool category key for PEGR to parse')
 parser.add_argument('--tool_parameters', dest='tool_parameters', help='Tool parameters that were set when producing the input dataset')
 parser.add_argument('--workflow_step_id', dest='workflow_step_id', default=None, help='Workflow step id')
 parser.add_argument('--user_email', dest='user_email', help='Current user email')
@@ -35,7 +38,19 @@ for input_txt in input_txts:
     statistics.append({})
     datasets.append(stats_util.get_datasets(args.config_file, input_id, input_datatype))
 
-payload = stats_util.get_base_json_dict(args.config_file, dbkey, args.history_id, args.history_name, args.stats_tool_id, args.stderr, args.tool_id, args.tool_parameters, args.user_email, args.workflow_step_id)
+input_forward_svgs = args.input_forward_svgs or []
+for input_forward_svg in input_forward_svgs:
+    file_path, hid, input_id, input_datatype, dbkey = input_forward_svg
+    statistics.append({})
+    datasets.append(stats_util.get_datasets(args.config_file, input_id, input_datatype))
+
+input_reverse_svgs = args.input_reverse_svgs or []
+for input_reverse_svg in input_reverse_svgs:
+    file_path, hid, input_id, input_datatype, dbkey = input_reverse_svg
+    statistics.append({})
+    datasets.append(stats_util.get_datasets(args.config_file, input_id, input_datatype))
+
+payload = stats_util.get_base_json_dict(args.config_file, dbkey, args.history_id, args.history_name, args.stats_tool_id, args.stderr, args.tool_id, args.tool_category, args.tool_parameters, args.user_email, args.workflow_step_id)
 payload['statistics'] = statistics
 payload['datasets'] = datasets
 payload['history_url'] = stats_util.get_history_url(args.config_file, args.history_id)
